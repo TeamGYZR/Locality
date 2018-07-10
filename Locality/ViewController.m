f5e5880c7073896e66bbb8966ca259a3aa23c838
Youngmin Shin <shinyoungmin@fb.com>
2018-07-09 22:05:31 -0700

    added .gitignore

---
+++ Locality/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Locality
+//
+//  Created by Ginger Dudley on 7/9/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
