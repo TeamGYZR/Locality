40300b8c2ed73fed40d2e1400a909bc53ae531e9
Ginger Dudley <gingerdudley@fb.com>
2018-07-09 18:24:19 -0700

    Initial Commit

---
+++ Locality/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Locality
+//
+//  Created by Ginger Dudley on 7/9/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
