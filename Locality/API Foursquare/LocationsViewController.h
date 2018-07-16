c51e4a80b3ed88f37f0b6e9ce1e45f4edc383e36
Ginger Dudley <gingerdudley@fb.com>
2018-07-16 12:20:09 -0700

    added locations storyboard and a button segue to this view

---
+++ Locality/API Foursquare/LocationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationsViewController.h
+//  Locality
+//
+//  Created by Ginger Dudley on 7/16/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface LocationsViewController : ViewController
+
+@end
