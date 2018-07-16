6dfbf0f9d0629ad5f9e56a95c7c389d598885f22
Youngmin Shin <shinyoungmin@fb.com>
2018-07-16 13:41:16 -0700

    added location feature that prints coordinates

---
+++ Locality/Youngmin/View Controllers/ExploreMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExploreMapViewController.h
+//  Locality
+//
+//  Created by Youngmin Shin on 7/16/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExploreMapViewController : UIViewController
+
+@end
---
+++ Locality/Youngmin/View Controllers/ExploreMapViewController.m
@@ -0,0 +1,45 @@
+//
+//  ExploreMapViewController.m
+//  Locality
+//
+//  Created by Youngmin Shin on 7/16/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import "ExploreMapViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface ExploreMapViewController () <MKMapViewDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation ExploreMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.mapView.delegate = self;
+    
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
