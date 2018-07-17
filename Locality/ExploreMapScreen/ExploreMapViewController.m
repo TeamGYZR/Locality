b2cdbb35412b6d455b2f07ef25f391d15d9a23b1
Youngmin Shin <shinyoungmin@fb.com>
2018-07-17 15:55:06 -0700

    added custom pins with image and button view

--- Locality/ExploreMapScreen/ExploreMapViewController.m
+++ Locality/ExploreMapScreen/ExploreMapViewController.m
@@ -18,6 +18,7 @@
 
 //- (void)locationManager:(CLLocationManager *)manager
 //     didUpdateLocations:(NSArray<CLLocation *> *)locations;
+-(void)goToCollection;
 
 @end
 
@@ -45,12 +46,7 @@
             
             [self findCurrentLocation];
         }
-  //  }
-    
-    //MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
-    
-    
-    //[self.mapView setRegion:sfRegion animated:false];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -69,12 +65,71 @@
     
     currentLocation = [locations lastObject];
     
+    
     NSLog(@"lat: %f, long: %f", currentLocation.coordinate.latitude, currentLocation.coordinate.longitude);
     
-    MKCoordinateRegion currentRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(currentLocation.coordinate.latitude, currentLocation.coordinate.longitude), MKCoordinateSpanMake(0.1, 0.1));
+    MKCoordinateRegion currentRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(currentLocation.coordinate.latitude, currentLocation.coordinate.longitude), MKCoordinateSpanMake(0.025, 0.025));
     
     
     [self.mapView setRegion:currentRegion animated:YES];
+    
+    
+    //CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude.floatValue, longitude.floatValue);
+    
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    CLLocationCoordinate2D pinCoordinates = CLLocationCoordinate2DMake(currentLocation.coordinate.latitude + 0.001, currentLocation.coordinate.longitude + 0.001);
+    annotation.coordinate = pinCoordinates;
+    annotation.title = @"Picture!";
+    [self.mapView addAnnotation:annotation];
+    
+    [self.navigationController popToViewController:self animated:YES];
+    self.mapView.showsUserLocation = YES;
+    
+    //[self.mapView.userLocation  ]
+    
+    
+}
+- (IBAction)getCurrentLocationTapped:(id)sender {
+    [self.locationManager requestLocation];
+    
+}
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    MKPinAnnotationView * annotationView = (MKPinAnnotationView *) [mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+    
+    if(annotationView == nil){
+        
+        annotationView =[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+        annotationView.canShowCallout = true;
+        annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+        annotationView.rightCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+    }
+    
+    UIImageView *imageView = (UIImageView*)annotationView.leftCalloutAccessoryView;
+    imageView.image = [UIImage imageNamed:@"frenchfries"];
+    
+    UIButton *collectionButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    [collectionButton addTarget:self action:@selector(goToCollection) forControlEvents:UIControlEventTouchUpInside];
+    [collectionButton setTitle:@"Hi" forState:UIControlStateNormal];
+    collectionButton.frame = CGRectMake(0.0, 0.0, 50.0, 50.0);
+    [collectionButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    
+    [annotationView.rightCalloutAccessoryView addSubview:collectionButton];
+    [annotationView.rightCalloutAccessoryView setUserInteractionEnabled:YES];
+    
+    return annotationView;
+    
+}
+
+- (void)makePins{
+    
+    
+}
+
+-(void)goToCollection{
+    NSLog(@"Went to collection view");
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
