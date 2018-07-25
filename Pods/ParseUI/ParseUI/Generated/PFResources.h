36c40103fea486e83b645081408ec15dc2d0dc63
Zelalem Tenaw Terefe <zterefe@fb.com>
2018-07-25 10:24:24 -0700

    added an upload option

--- Pods/ParseUI/ParseUI/Generated/PFResources.h
+++ Pods/ParseUI/ParseUI/Generated/PFResources.h
@@ -11,14 +11,6 @@
 + (NSData *)parse_logo3x_png;//modified:2018-07-12 19:39:31 -0700
 + (NSData *)facebook_icon_png;//modified:2018-07-12 19:39:31 -0700
 + (NSData *)twitter_icon_png;//modified:2018-07-12 19:39:31 -0700
-+ (NSData *)twitter_icon3x_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)facebook_icon2x_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)parse_logo_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)facebook_icon3x_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)twitter_icon2x_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)parse_logo2x_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)parse_logo3x_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)facebook_icon_png;//modified:2018-07-10 10:51:48 -0700
-+ (NSData *)twitter_icon_png;//modified:2018-07-10 10:51:48 -0700
+
 
 @end
