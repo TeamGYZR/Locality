74b089b4b4897b3160e0e1888fe9eae4554eb440
Zelalem Tenaw Terefe <zterefe@fb.com>
2018-07-27 13:16:32 -0700

    Added home storyboard

---
+++ Locality/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Locality
+//
+//  Created by Zelalem Tenaw Terefe on 7/27/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
