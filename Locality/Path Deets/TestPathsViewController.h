be10fdebbb8714388d21a4dd905a9ff20e131c31
Youngmin Shin <shinyoungmin@fb.com>
2018-07-30 08:46:41 -0700

    testing making polyline in map

---
+++ Locality/Path Deets/TestPathsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestPathsViewController.h
+//  Locality
+//
+//  Created by Youngmin Shin on 7/29/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestPathsViewController : UIViewController
+
+@end
