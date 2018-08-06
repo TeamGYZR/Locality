96d2bf4d2e6107bb6640f646f3cb56badf2b2cba
Ginger Dudley <gingerdudley@fb.com>
2018-08-06 11:52:46 -0700

    added tab bar for profile page

---
+++ Locality/Profile/ProfilePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfilePageViewController.h
+//  Locality
+//
+//  Created by Ginger Dudley on 8/6/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfilePageViewController : UIViewController
+
+@end
