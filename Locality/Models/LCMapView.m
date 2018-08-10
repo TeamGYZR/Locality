a5435affea9fd3777af8bdf08b7e2eefc9501594
Ginger Dudley <gingerdudley@fb.com>
2018-08-10 09:30:14 -0700

    added different colored pins to lcmapview annotation view

--- Locality/Models/LCMapView.m
+++ Locality/Models/LCMapView.m
@@ -159,6 +159,13 @@
                 annotationView.leftCalloutAccessoryView.userInteractionEnabled=YES;
                 annotationView.canShowCallout = YES;
             }];
+            if ([pinAnnotation.pinCategory isEqualToString:@"Foodie"]) {
+                annotationView.pinTintColor = [UIColor blueColor];
+            } else if ([pinAnnotation.pinCategory isEqualToString:@"Entertainment"]){
+                annotationView.pinTintColor = [UIColor redColor];
+            } else if ([pinAnnotation.pinCategory isEqualToString:@"Nature"]){
+                annotationView.pinTintColor = [UIColor grayColor];
+            }
         }
         return annotationView;
     }
